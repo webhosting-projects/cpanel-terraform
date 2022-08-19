f91c0033482ee2f3b32508dd0b34b0ea531f4e8f
danielitogomez <daniel.gomez.amaran@gmail.com>
2022-08-18 22:09:23 -0400

    feat: update script

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # cpanel-terraform
 This repo is for deploy cpanel infrastructure in AWS.
+
+# Installation
+yum update -y
+yum install wget -y
+yum install perl -y
+curl -o latest -L https://securedownloads.cpanel.net/latest
+sh latest
--- scripts/install_cpanel.sh
+++ scripts/install_cpanel.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-#cpanel
 sudo yum update -y
-sudo yum install wget -y
-sudo yum install perl -y
-cd /home 
-wget –N http://httpupdate.cpanel.net/latest
-chmod +x latest 
-sh latest
-
-#Softaculous
-wget -N http://files.softaculous.com/install.sh
-chmod 755 install.sh
-./install.sh
+sudo yum install htop -y
+sudo yum install git -y
